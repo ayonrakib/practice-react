d8bcbe2d073a79c7af825f0b42e618dd5c653717
MSI\rakib <ayonrakib@gmail.com>
2021-04-29 15:19:01 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+
+class Car extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      brand: "Kia",
+      model: "Forte",
+      color: "Black",
+      year: "2020"
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render(){
+    return(
+      <div>
+        <h1>The brand of my car is {this.state.brand}.</h1>
+        <h2>The color of my car is {this.state.color}.</h2>
+        <h3>The model of my car is {this.state.model}.</h3>
+        <h4>The year of my car is {this.state.year}.</h4>
+        <button type = "button" onClick = {this.changeColor}>
+          Change Color!
+        </button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
@@ -21,5 +51,5 @@ function App() {
     </div>
   );
 }
-
+ReactDOM.render(<Car />, document.getElementById('root'));
 export default App;
