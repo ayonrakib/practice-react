e5a25658e1e291b9ef096bf4178e459fdc136eb2
MSI\rakib <ayonrakib@gmail.com>
2021-04-30 17:23:43 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -2,34 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-
-class Car extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      brand: "Kia",
-      model: "Forte",
-      color: "Black",
-      year: "2020"
-    };
-  }
-  changeColor = () => {
-    this.setState({color: "blue"});
-  }
-  render(){
-    return(
-      <div>
-        <h1>The brand of my car is {this.state.brand}.</h1>
-        <h2>The color of my car is {this.state.color}.</h2>
-        <h3>The model of my car is {this.state.model}.</h3>
-        <h4>The year of my car is {this.state.year}.</h4>
-        <button type = "button" onClick = {this.changeColor}>
-          Change Color!
-        </button>
-      </div>
-    )
-  }
-}
+import { render } from '@testing-library/react';
 
 function App() {
   return (
@@ -51,5 +24,5 @@ function App() {
     </div>
   );
 }
-ReactDOM.render(<Car />, document.getElementById('root'));
+
 export default App;
---
+++ src/component.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+function Example(){
+    const [count, setCount] = useState(0);
+    return(
+        <div>
+            <p>
+                You clicked this {count} times!
+            </p>
+            <button onClick = {() => setCount(count+1)}>
+                Click me
+            </button>
+        </div>
+    )
+    
+}
+
+
+export default Example;
